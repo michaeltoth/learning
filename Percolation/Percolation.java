e7a03bdaa67aa99c8f4bea1e6bc3359ab494c8db
Michael Toth <mt.toth@gmail.com>
2014-09-08 00:02:19 -0400

    Basic implementation of simpler methods

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -1,8 +1,14 @@
 public class Percolation {
-   public Percolation(int N)                // create N-by-N grid, with all sites blocked
+   public Percolation(int N)                // create N-by-N grid, with all sites blocked (false)
+       boolean[][] a = new boolean[N][N];
    public void open(int i, int j)           // open site (row i, column j) if it is not already
+       a[i][j] = True;
+       // Union Operation
    public boolean isOpen(int i, int j)      // is site (row i, column j) open?
+       return a[i][j] == True
    public boolean isFull(int i, int j)      // is site (row i, column j) full?
+       // Find Operation
    public boolean percolates()              // does the system percolate?
+       // Union top to bottom
    public static void main(String[] args)   // test client, optional
 }
