a614de6b7ee5fc7d7ee973e0abced5cf71b6c4b5
Michael Toth <mt.toth@gmail.com>
2014-09-11 22:45:32 -0400

    Solved backwash issue with second union-find object

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -2,73 +2,107 @@ import java.util.Arrays;
 
 public class Percolation {
    private int size;
-   private boolean[][] op;
-   private int[][] id;
+   //private boolean percolatesFlag;
    private WeightedQuickUnionUF uf;
+   private WeightedQuickUnionUF topUF;
+   private boolean[][] opened;
+   //private boolean[][] connectedToBottom;
    
+   // Converts 2d (x, y) coordinates to single value, needed for union find object
    private int xyTo1D(int i, int j) {
        return size * (i - 1) + j;
    }
    
-   private void check_bounds(int i, int j) {
+   private void checkBounds(int i, int j) {
        if (i <= 0 || i > size) throw new IndexOutOfBoundsException("row index i out of bounds");
        if (j <= 0 || j > size) throw new IndexOutOfBoundsException("column index j out of bounds");
    }
    
-   public Percolation(int N) {               // create N-by-N grid, with all sites blocked (false)
+   // create N-by-N grid, with all sites blocked (false)
+   public Percolation(int N) {               
        if (N <= 0) throw new IllegalArgumentException("Grid size must be > 0");
-       op = new boolean[N + 1][N + 1];
-       size = N;
-       id = new int[N + 1][N + 1];  // This will make it easier to deal with 0-based indexing
        
-       for (int i = 0; i < id.length; i++) {
-           for (int j = 0; j < id[i].length; j++) {
-               id[i][j] = xyTo1D(i,j);
-           }
-       }
-       
-       // Add 2 for top and bottom virtual sites
+       // Union find library for operations. Add 2 for top and bottom virtual sites.
        uf = new WeightedQuickUnionUF(N * N + 2);
+       topUF = new WeightedQuickUnionUF(N * N + 2);
+   
+       /* 
+          Boolean array to indicate if opened.
+          Size is N + 1 and 0-indexed values will not be used. Program will be
+          passed 1-indexed values, but Java uses 0 indexing. This structure 
+          makes code cleaner at the expense of a slight hit to memory.
+       */
+       opened = new boolean[N + 1][N + 1];
+       //connectedToBottom = new boolean[N + 1][N + 1];
+       size = N;
+       
+       // Mark bottom row as connected to bottom
+       //for (int i = 0; i < connectedToBottom[N].length; i++) {
+       //    connectedToBottom[N][i] = true;
+       //}
    }
        
-   public void open(int i, int j) {           // open site (row i, column j) if it is not already
-       check_bounds(i,j);
-       op[i][j] = true;
+   public void open(int i, int j) {
+       checkBounds(i, j);
+       
+       // Mark open in boolean array
+       opened[i][j] = true;
        
        // Connect with open neighbors
-       if (i != 1     && isOpen(i-1, j  )) { uf.union(id[i][j], id[i-1][j  ]); }
-       if (i != size  && isOpen(i+1, j  )) { uf.union(id[i][j], id[i+1][j  ]); }
-       if (j != 1     && isOpen(i  , j-1)) { uf.union(id[i][j], id[i  ][j-1]); }
-       if (j != size  && isOpen(i  , j+1)) { uf.union(id[i][j], id[i  ][j+1]); }
+       if (i != 1 && isOpen(i-1, j)) { 
+           uf.union(xyTo1D(i, j), xyTo1D(i-1, j));
+           topUF.union(xyTo1D(i, j), xyTo1D(i-1, j));
+           //if (connectedToBottom[i-1][j]) { connectedToBottom[i][j] = true; }
+           //else if (connectedToBottom[i][j]) { connectedToBottom[i-1][j] = true; }
+       }
+       if (i != size && isOpen(i+1, j)) { 
+           uf.union(xyTo1D(i, j), xyTo1D(i+1, j));
+           topUF.union(xyTo1D(i, j), xyTo1D(i+1, j));
+           //if (connectedToBottom[i+1][j]) { connectedToBottom[i][j] = true; }
+           //else if (connectedToBottom[i][j]) { connectedToBottom[i+1][j] = true; }
+       }
+       if (j != 1 && isOpen(i, j-1)) { 
+           uf.union(xyTo1D(i, j), xyTo1D(i, j-1));
+           topUF.union(xyTo1D(i, j), xyTo1D(i, j-1));
+           //if (connectedToBottom[i][j-1]) { connectedToBottom[i][j] = true; }
+           //else if (connectedToBottom[i][j]) { connectedToBottom[i][j-1] = true; }
+       }
+       if (j != size && isOpen(i, j+1)) { 
+           uf.union(xyTo1D(i, j), xyTo1D(i, j+1));
+           topUF.union(xyTo1D(i, j), xyTo1D(i, j+1));
+           //if (connectedToBottom[i][j+1]) { connectedToBottom[i][j] = true; }
+           //else if (connectedToBottom[i][j]) { connectedToBottom[i][j+1] = true; }
+       }
        
        // If in top or bottom row, connect to virtual sites
-       if (i == 1) { uf.union(id[i][j], 0); }
-       if (i == size) { uf.union(id[i][j], size * size + 1); }
+       if (i == 1) { 
+           uf.union(xyTo1D(i, j), 0); 
+           topUF.union(xyTo1D(i, j), 0); 
+       }
+       if (i == size) { 
+           uf.union(xyTo1D(i, j), size * size + 1); 
+       }
+       //if (isFull(i,j) && connectedToBottom[i][j]) {
+       //    percolatesFlag = true;
+       //}
+       //System.out.println("("+i+","+j+") connected? "+connectedToBottom[i][j]);
+       
    }
    
-   public boolean isOpen(int i, int j) {      // is site (row i, column j) open?
-       check_bounds(i,j);
-       return op[i][j];  // Dealing with 0-based indexing and 1-based requests to function
+   public boolean isOpen(int i, int j) {
+       checkBounds(i, j);
+       return opened[i][j];
    }
    
-   public boolean isFull(int i, int j) {      // is site (row i, column j) full?
-       check_bounds(i,j);
-       return uf.connected(id[i][j], 0);
+   public boolean isFull(int i, int j) {
+       checkBounds(i, j);
+       return topUF.connected(xyTo1D(i, j), 0); // Full if connected to top virtual site
    }
    
-   public boolean percolates() {              // does the system percolate?
+   public boolean percolates() {
        return uf.connected(0, size * size + 1);
-       // Union top to bottom
    }
-   public static void main(String[] args) {   // test client, optional
-       Percolation my_perc = new Percolation(3);
-       System.out.println(Arrays.deepToString(my_perc.id));
-       System.out.println(Arrays.deepToString(my_perc.op));
-       my_perc.open(3,2);
-       System.out.println("Opened First One");
-       my_perc.open(3,3);
-       System.out.println(my_perc.uf.connected(9,8));
-       System.out.println(Arrays.deepToString(my_perc.id));
-       System.out.println(Arrays.deepToString(my_perc.op));
+   public static void main(String[] args) {
+       
    }
 }
