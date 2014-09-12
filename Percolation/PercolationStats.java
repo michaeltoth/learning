70b9dc12765d53b13844733b9f481383e619b378
Michael Toth <mt.toth@gmail.com>
2014-09-11 22:46:11 -0400

    Working.  Need to check on calculation error

--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -1,64 +1,70 @@
 public class PercolationStats {
-    private double[] site_fraction;
-    private Percolation perc;
-    private int grid_sz;
-    private int num_runs;
+    // Instance variables to use in computations.
+    private double[] siteFraction;
+    private int numRuns;
     
-    private void open_random() {
-        int i = StdRandom.uniform(grid_sz) + 1;
-        int j = StdRandom.uniform(grid_sz) + 1;
-        if (perc.isOpen(i,j)) { open_random(); }
-        else { perc.open(i,j); return; }
+    private void openRandom(int gridSize, Percolation perc) { // Open a site uniformly at random among closed sites
+        int i = StdRandom.uniform(gridSize) + 1;
+        int j = StdRandom.uniform(gridSize) + 1;
+        
+        // If site is already open, rerun this function.  Otherwise, open the site.
+        if (perc.isOpen(i, j)) { 
+            openRandom(gridSize, perc); 
+        }
+        else { 
+            perc.open(i, j); 
+            return; 
+        }
     }
     
     public PercolationStats(int N, int T) {    // perform T independent computational experiments on an N-by-N grid
         if (N <= 0 || T <= 0) throw new IllegalArgumentException("N and T must be > 0");
         
-        site_fraction = new double[T];
-        grid_sz = N;
-        num_runs = T;
+        // Instantiating variables based on parameters N and T
+        siteFraction = new double[T];
+        numRuns = T;
         
         for (int i = 0; i < T; i++) {
-            perc = new Percolation(N);
-            int c = 0;
-            while (perc.percolates() == false) {
-                open_random();
-                c++;
+            Percolation perc = new Percolation(N);
+            int count = 0;
+            
+            // Open sites until the system percolates, counting the number of sites opened
+            while (!perc.percolates()) {
+                openRandom(N, perc);
+                count++;
             }
-            site_fraction[i] = (float)c/(N*N);
+            
+            // Record the fraction of open sites for experiment i
+            siteFraction[i] = (float) count/(N*N);
         }
     }
     public double mean() {                     // sample mean of percolation threshold
-        double sum = 0.0;
-        for (double i: site_fraction)
-            sum += i;
-        return sum/num_runs;
+        return StdStats.mean(siteFraction);
     }
     
     public double stddev() {                   // sample standard deviation of percolation threshold
-        double var_sum = 0.0;
-        double mu = mean();
-        for (double i: site_fraction)
-            var_sum = Math.pow((i - mu),2.0);
-        double variance = var_sum/(num_runs - 1);
-        return Math.sqrt(variance);
+        return StdStats.stddev(siteFraction);
     }
     
     public double confidenceLo() {             // returns lower bound of the 95% confidence interval
         double mu = mean();
         double sigma = stddev();
-        return (mu - (1.96 * sigma / Math.sqrt(num_runs)));
+        return (mu - (1.96 * sigma / Math.sqrt(numRuns)));
     }
     
     public double confidenceHi() {             // returns upper bound of the 95% confidence interval
         double mu = mean();
         double sigma = stddev();
-        return (mu + (1.96 * sigma / Math.sqrt(num_runs)));
+        return (mu + (1.96 * sigma / Math.sqrt(numRuns)));
     }
-    public static void main(String[] args) {   // test client, described below
+    public static void main(String[] args) {
+        // Read N (grid size) and T (number of samples) from standard input
         int N = StdIn.readInt();
         int T = StdIn.readInt();
+        
+        // Make PercolationStats object to runs statistics on N*N grid for T samples
         PercolationStats ps = new PercolationStats(N, T);
+        
         System.out.println("mean = " + ps.mean());
         System.out.println("stddev = " + ps.stddev());
         System.out.println("95% confidence interval = " + ps.confidenceLo() + ", " + ps.confidenceHi());
