7532e49f72b126259b9e69e28a450d9c75809826
Michael Toth <mt.toth@gmail.com>
2014-09-26 14:35:30 -0400

    First commit.  Read csv

---
+++ R/Ex3/rankall.R
@@ -0,0 +1,11 @@
+rankall <- function(outcome, num = "best") {
+    ## Read outcome data
+    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+    ## Check that state and outcome are valid
+
+    ## For each state, find the hospital of the given rank
+
+    ## Return a data frame with the hospital names and the abbreviated state name
+
+}
---
+++ R/Ex3/rankhospital.R
@@ -0,0 +1,9 @@
+rankhospital <- function(state, outcome, num = "best") {
+    ## Read outcome data
+    date <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+    ## Check that state and outcome are valid
+
+    ## Return hospital name in that state with the given rank 30-day death rate
+
+}
