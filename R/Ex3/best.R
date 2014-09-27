9645d3d7d2c4d179fdeea64308bd7e48f3c0d770
Michael Toth <mt.toth@gmail.com>
2014-09-27 19:23:05 +0000

    Need to fix issues with NA and sort alphabetically

--- R/Ex3/best.R
+++ R/Ex3/best.R
@@ -1,6 +1,7 @@
 best <- function(state, outcome) {
     ## Read outcome data
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    data[, 11] <- as.numeric(data[, 11]); data[, 17] <- as.numeric(data[, 17]); data[, 23] <- as.numeric(data[, 23])
 
     ## Check that state and outcome are valid
     potentialOutcomes <- c("heart attack","heart failure","pneumonia")
@@ -9,5 +10,15 @@ best <- function(state, outcome) {
 
     ## Return hospital name in that state with lowest 30-day death rate
     ## Heart attack = col 11, heart failure = col 17, pneumonia = col 23
+    ## Subset the data based on requested state
 
+    dataSubset <- data[(data[,7] == state),]
+    
+    if (outcome == "heart attack") { colIndex <- 11 }
+    else if (outcome == "heart failure") { colIndex <- 17 }
+    else if (outcome == "pneumonia") { colIndex <- 23 }
+    
+    lowestRate <- min(dataSubset[,colIndex], na.rm = TRUE)
+    hospitals <- dataSubset[(dataSubset[,colIndex] == lowestRate),2]
+    cat(hospitals, "\n")
 }
