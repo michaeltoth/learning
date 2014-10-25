5c864bdcfd5f9aeaa5f7a595d64a0fea01dbc323
Michael Toth <mt.toth@gmail.com>
2014-10-25 13:03:10 -0400

    Removed unneccessary file loads

--- GettingCleaning/Project/run_analysis.R
+++ GettingCleaning/Project/run_analysis.R
@@ -3,15 +3,12 @@ setwd("/home/michael/Documents/Projects/Coursera/Data Science/GettingCleaning/Pr
 # Loading in train and test data
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 # Combining test and training datasets
 X_all <- rbind(X_test, X_train)
 y_all <- rbind(y_test, y_train)
-subject_all <- rbind(subject_test, subject_train)
 
 # Read in feature names and activity names
 features <- read.table("UCI HAR Dataset/features.txt")
