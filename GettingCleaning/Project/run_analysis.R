4e07c7337cc61103be1b94465b7b325cbda11488
Michael Toth <mt.toth@gmail.com>
2014-11-08 16:59:28 -0500

    Added subject to tidy dataset

--- GettingCleaning/Project/run_analysis.R
+++ GettingCleaning/Project/run_analysis.R
@@ -1,14 +1,16 @@
-setwd("/home/michael/Documents/Projects/Coursera/Data Science/GettingCleaning/Project")
-
 # Loading in train and test data
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+sub_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+sub_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
 
 # Combining test and training datasets
 X_all <- rbind(X_test, X_train)
 y_all <- rbind(y_test, y_train)
+subject_all <- rbind(sub_test, sub_train)
 
 # Read in feature names and activity names
 features <- read.table("UCI HAR Dataset/features.txt")
@@ -24,16 +26,21 @@ matches <- grep(paste(toMatch,collapse="|"),features$V2,value=TRUE)
 # Extract only the measurements on the mean and standard deviation
 X_all_relevant <- X_all[,matches]
 
-# Add activity lables from y_all file
-X_all_labeled <- cbind(y_all, X_all_relevant)
-colnames(X_all_labeled)[1] <- "Activity"
+# Add activity and subject labels from y_all file and subject_all file
+X_all_labeled <- cbind(y_all, subject_all, X_all_relevant)
+colnames(X_all_labeled)[1:2] <- c("Activity","Subject")
 
-# Merge the activity names from y with the activity labels in X to form data set
+# NEED TO ADD DESCRIPTIVE LABELS FOR OTHERS
+
+# Merge the activity names from y with the activity labels in X as descriptors
 mergedData <- merge(X_all_labeled, activities,by.x="Activity", 
                     by.y="V1",all=TRUE)[,-1]
-colnames(mergedData)[67] <- "Activity"
+colnames(mergedData)[68] <- "Activity"
 
-# Determine mean of each feature by activity:
-output <- aggregate(mergedData[,1:66],by=list(mergedData$Activity),mean)
+# Determine mean of each feature by activity and subject:
+output <- aggregate(mergedData[,2:67],
+            by=list(mergedData$Activity,mergedData$Subject),
+            mean)
+colnames(output)[1:2] <- c("Activity","Subject")
 
 write.table(output, file = "tidy.txt", row.names=FALSE)
