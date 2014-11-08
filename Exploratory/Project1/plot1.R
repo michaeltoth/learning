a9657df42bee98738e360b5cf7c66668ae770a43
Michael Toth <mt.toth@gmail.com>
2014-11-08 12:17:11 -0500

    Finalized plot 1 code and image

--- Exploratory/Project1/plot1.R
+++ Exploratory/Project1/plot1.R
@@ -1,14 +1,28 @@
 # Exercise assumes you have downloaded and extracted the zip file
-# and that household_power_consumption.txt is in the working directory
+# household_power_consumption_dated.txt is a pre-subsetted file by date
+# household_power_consumption_dated.txt must be in the working directory
 
+# Load in file that has already been subsetted to Feb 1-2 2007
 if ( !exists("housePower")) {
-    housePower <- read.table("household_power_consumption.txt", sep=";", 
-            header = TRUE)
+    housePower <- read.table("household_power_consumption_dated.txt", 
+        sep = ";", 
+        header = TRUE)
+    
+    # Add a datetime column that combines the date and time columns
+    housePower$Datetime <- as.POSIXct(paste(housePower$Date, housePower$Time), 
+        format = "%d/%m/%Y %H:%M:%S")
 }
 
-# Subset the dataset to only include 2/1/2007 and 2/2/2007 data
-housePower <- housePower[housePower$Date %in% c("1/2/2007","2/2/2007"),]
+# Set graphics device to png
+png(filename = "plot1.png", width = 480, height = 480)
 
-# Convert date and time fields.  Currently incorrect
-housePower$Date <- as.Date(as.character(housePower$Date), format="%d/%m/%Y")
-housePower$Time <- strptime(housePower$Time, format="%H:%M:%S")
+# Set parameters and create plot1
+par(mfrow = c(1,1))
+hist(housePower$Global_active_power, 
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     breaks = 15)
+
+# Turn off png graphics device
+dev.off()
