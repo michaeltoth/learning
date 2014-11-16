ae57714f4158a0a2c810bf2425b302da4e116f40
Michael Toth <mt.toth@gmail.com>
2014-11-16 18:12:46 -0500

    Converted plot to use ggplot2

--- Exploratory/Project2/plot3.R
+++ Exploratory/Project2/plot3.R
@@ -21,11 +21,9 @@ if (!exists("SCC")) { SCC <- readRDS("data/Source_Classification_Code.rds") }
 # Subset Baltimore NEI Data
 NEI_Baltimore <- NEI[NEI$fips == "24510",]
 
-# Sum total emissions by year from all sources
-baltimoreEmissions <- aggregate(NEI_Baltimore$Emissions, 
-                                by = list(NEI_Baltimore$year), sum)
-colnames(baltimoreEmissions) <- c("Year", "Emissions")
+# Sum total emissions by year by type
+baltimoreEmissionsType <- aggregate(Emissions ~ year+type, 
+                                    data = NEI_Baltimore, sum)
 
-plot(x = baltimoreEmissions$Year, y = baltimoreEmissions$Emissions,
-     xlim = c(1998,2008), main = "Baltimore Yearly Emissions", 
-     ylab = expression("PM"[2.5]*" Emissions"), xlab = "Year")
+ggplot(data = baltimoreEmissionsType, aes(year, Emissions)) + 
+    geom_line(aes(color=type))
