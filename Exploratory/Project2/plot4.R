b239cc79f3dc51614c6d5192af1b61361355fb32
Michael Toth <mt.toth@gmail.com>
2014-11-16 18:13:00 -0500

    Added code for plot 4

---
+++ Exploratory/Project2/plot4.R
@@ -0,0 +1,34 @@
+# Create data directory
+if (!file.exists("data/")) { dir.create("data") }
+
+# Download and extract data.  This may take a minute or two.
+file1 = "data/Source_Classification_Code.rds"
+file2 = "data/summarySCC_PM25.rds"
+if (!file.exists(file1) | !file.exists(file2)) {
+    # Download file from course site
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    download.file(fileUrl, destfile = "data/FNEI_data.zip", method="curl")
+    dateDownloaded <- date()
+    
+    # Extract file contents
+    unzip("data/FNEI_data.zip", exdir = "data")
+}
+
+# Read files into R using readRDS.  This may take a minute or two
+if (!exists("NEI")) { NEI <- readRDS("data/summarySCC_PM25.rds") }
+if (!exists("SCC")) { SCC <- readRDS("data/Source_Classification_Code.rds") }
+
+# After some exploration, I found that both grep("Coal", SCC$Short.Name) and
+# union(grep("Coal", SCC$SCC.Level.Three), grep("Coal", SCC$SCC.Level.Four))
+# return the same list of values, all associated with coal combustion.  This
+# deliberately excludes charcoal combustion sources.
+
+# Extract only those observations from coal combustion sources
+SCC_Coal <- SCC[grep("Coal", SCC$Short.Name),]
+NEI_Coal <- NEI[NEI$SCC %in% SCC_Coal$SCC,]
+
+# Sum total emissions by year from all sources
+coalEmissions <- aggregate(Emissions ~ year, data = NEI_Coal, sum)
+
+ggplot(data = coalEmissions, aes(year, Emissions)) + geom_line()
+
